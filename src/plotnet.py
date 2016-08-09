e204e624dc936cb73d1998d058a18b126ea44884
Yifeng Tao <taoyf12@sina.com>
2016-08-09 17:01:15 -0400

    show the network

---
+++ src/plotnet.py
@@ -0,0 +1,34 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+def draw_graph(graph, path_pdf):
+    pp = PdfPages(path_pdf)
+    # extract nodes from graph
+    nodes = set([n1 for n1, n2 in graph] + [n2 for n1, n2 in graph])
+
+    # create networkx graph
+    G=nx.Graph()
+
+    # add nodes
+    for node in nodes:
+        G.add_node(node)
+
+    # add edges
+    for edge in graph:
+        G.add_edge(edge[0], edge[1])
+
+    # draw graph
+    pos = nx.shell_layout(G)
+    nx.draw(G, pos)
+
+    # show graph
+    #plt.show()
+    pp.savefig()
+    pp.close()
+# draw example
+
+graph = [(20, 21),(21, 22),(22, 23), (23, 24),(24, 25), (25, 20), (20, 20)]
+path_pdf = 'network.pdf'
+draw_graph(graph, path_pdf)
+print 'Done!'
