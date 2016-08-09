a6c4bc2c918189facfab8f2b1189cb44a385ab07
Yifeng Tao <taoyf12@sina.com>
2016-08-09 19:38:23 -0400

    works fine

--- src/plotnet_format.py
+++ src/plotnet_format.py
@@ -1,13 +1,16 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 def draw_graph(graph, labels=None, graph_layout='shell',
                node_size=16, node_color='blue', node_alpha=0.3,
                node_text_size=12,
                edge_color='blue', edge_alpha=0.3, edge_tickness=1,
                edge_text_pos=0.3,
-               text_font='sans-serif'):
+               text_font='sans-serif',
+               path_pdf = 'network.pdf'):
 
+    pp = PdfPages(path_pdf)
     # create networkx graph
     G=nx.Graph()
 
@@ -17,33 +20,38 @@ def draw_graph(graph, labels=None, graph_layout='shell',
 
     # these are different layouts for the network you may try
     # shell seems to work best
+    print 'calculating position...'
     if graph_layout == 'spring':
         graph_pos=nx.spring_layout(G)
     elif graph_layout == 'spectral':
         graph_pos=nx.spectral_layout(G)
     elif graph_layout == 'random':
         graph_pos=nx.random_layout(G)
+    elif graph_layout == 'graphviz':
+        graph_pos=nx.graphviz_layout(G)
     else:
         graph_pos=nx.shell_layout(G)
 
+    print 'plotting network...'
     # draw graph
     nx.draw_networkx_nodes(G,graph_pos,node_size=node_size, 
                            alpha=node_alpha, node_color=node_color)
     nx.draw_networkx_edges(G,graph_pos,width=edge_tickness,
                            alpha=edge_alpha,edge_color=edge_color)
-    nx.draw_networkx_labels(G, graph_pos,font_size=node_text_size,
-                            font_family=text_font)
+    #nx.draw_networkx_labels(G, graph_pos,font_size=node_text_size,
+    #                        font_family=text_font)
 
-    if labels is None:
-        labels = range(len(graph))
+    #if labels is None:
+    #    labels = range(len(graph))
 
-    edge_labels = dict(zip(graph, labels))
-    nx.draw_networkx_edge_labels(G, graph_pos, edge_labels=edge_labels, 
-                                 label_pos=edge_text_pos)
+    #edge_labels = dict(zip(graph, labels))
+    #nx.draw_networkx_edge_labels(G, graph_pos, edge_labels=edge_labels, 
+    #                             label_pos=edge_text_pos)
 
     # show graph
-    plt.show()
-
+    #plt.show()
+    pp.savefig()
+    pp.close()
 
 if __name__ == '__main__':
 
@@ -58,17 +66,19 @@ if __name__ == '__main__':
         genex = values[1].lower()
         geney = values[2].lower()
         #print genex, geney
-        if i % 1000 == 1:
+        if i % 500 == 1:
+            print 'proceed: {}%'.format(100.0*i/155900)
             graph.append((genex,geney))
     print 'drawing network...'
     path_pdf = 'network.pdf'
-    node_size = 100
-    draw_graph(graph, path_pdf,node_size)
-    print 'Done!'
+    #node_size = 100
+    #draw_graph(graph, path_pdf,node_size)
+    
 
     # you may name your edge labels
-    labels = map(chr, range(65, 65+len(graph)))
+    #labels = map(chr, range(65, 65+len(graph)))
     #draw_graph(graph, labels)
 
     # if edge labels is not specified, numeric labels (0, 1, 2...) will be used
-    draw_graph(graph)
+    draw_graph(graph,graph_layout='spring',path_pdf = path_pdf)
+    print 'Done!'
