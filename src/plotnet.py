1451dcc4819a8d778f0e388420a6f4528605d2bb
Yifeng Tao <taoyf12@sina.com>
2016-08-09 18:30:52 -0400

    my network

--- src/plotnet.py
+++ src/plotnet.py
@@ -28,7 +28,23 @@ def draw_graph(graph, path_pdf):
     pp.close()
 # draw example
 
-graph = [(20, 21),(21, 22),(22, 23), (23, 24),(24, 25), (25, 20), (20, 20)]
-path_pdf = 'network.pdf'
-draw_graph(graph, path_pdf)
-print 'Done!'
+
+if __name__ == '__main__':
+
+    graph = []
+    # [(20, 21),(21, 22),(22, 23), (23, 24),(24, 25), (25, 20), (20, 20)]
+    path_graph = '../pathway/pathway.graph'
+    print 'reading from: {}...'.format(path_graph)
+    i = 0
+    for line in open(path_graph, 'r'):
+        i += 1
+        values = line.strip().split('\t')
+        genex = values[1].lower()
+        geney = values[2].lower()
+        #print genex, geney
+        if i % 1000 == 1:
+            graph.append((genex,geney))
+    print 'drawing network...'
+    path_pdf = 'network.pdf'
+    draw_graph(graph, path_pdf)
+    print 'Done!'
