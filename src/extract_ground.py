7153b29ec17e1fa77b7fb40590fea967751c2d84
Yifeng Tao <taoyf12@sina.com>
2016-08-21 14:04:25 -0400

    looks nice

--- src/extract_ground.py
+++ src/extract_ground.py
@@ -1,10 +1,13 @@
-# Refine the data.
+# Extract positive grounded examples.
 from collections import defaultdict as dd
 import io
 import os
 import random
 
 def readsolution(path):
+    '''
+    read into (sga,deg) tuples from file.
+    '''
     print 'reading from: {}...'.format(path)
     deg = set()
     sga2deg_all = set()
@@ -22,12 +25,14 @@ def readsolution(path):
     return sga2deg_all
 
 def readsga2deg(path):
+    '''
+    read into (sga,deg) tuples from file.
+    '''
     print 'reading from: {}...'.format(path)
     sga2deg = set()
     for line in open(path, 'r'):
         values = line.strip().split('\t')
         sga2deg.add((values[0],values[1]))
-    #print 'len = {}'.format(len(sga2deg))
     return sga2deg
 
 def save2txt_list(path, table):
@@ -42,22 +47,20 @@ def save2txt_list(path, table):
     f.close()
 
 def extract_ground_in(path_solution, path_sga2deg, path_out, is_train, path_label=None):
-    
+    '''
+    extract grounded positive solutions.
+    path_solution: *.solutions.txt file, contains grounded examples.
+    path_sga2deg: contrain positive (un)grounded positive examples.
+    path_out: output file contains grounded positive examples.
+    is_train: if it is training set.
+    path_label: if it is training set, we compress the isDEG corpus.
+    '''
     # all the grounded examples.
     sga2deg_all = readsolution(path_solution)
-
     # positive examples.
     sga2deg = readsga2deg(path_sga2deg)
-
     # positive grounded examples.
     sga2deg = sga2deg.intersection(sga2deg_all)
-    # for pair in sga2deg_all:
-    #     if pair in sga2deg:
-    #         sga2deg_out.add(pair)
-
-    # postive ones
-    # sga2deg = sga2deg_out
-    print 'len(sga2deg) = {}'.format(len(sga2deg))
 
     deg_corpus = set()
     for pair in sga2deg:
@@ -69,8 +72,6 @@ def extract_ground_in(path_solution, path_sga2deg, path_out, is_train, path_labe
         for gene in deg_corpus:
             print >> f, 'isDEG\t'+gene
         f.close
-    print 'len(deg_corpus) = {}'.format(len(deg_corpus))
-
 
     sga2deg_list = dd(list)
     for line in sga2deg:
@@ -108,26 +109,44 @@ def extract_ground_in(path_solution, path_sga2deg, path_out, is_train, path_labe
     os.remove(path_all)
 
     save2txt_list(path_out,examples)
-    return deg_corpus,sga2deg
+    return deg_corpus,sga2deg,set(sga2deg_list.keys())
 
 
 if __name__ == '__main__':
     dest = '../pathway_forw_patient'
-    deg_corpus_train,sga2deg_train = extract_ground_in(dest+'/pathway_origin/train.solutions.txt', \
+    deg_corpus_train,sga2deg_train,sga_corpus_train = extract_ground_in(dest+'/pathway_origin/train.solutions.txt', \
         dest+'/pathway_origin/sga2deg_train', dest+'/pathway_ground/train.examples', True, \
         dest+'/pathway_ground/labels.cfacts')
 
-    deg_corpus_test,sga2deg_test = extract_ground_in(dest+'/pathway_origin/test.solutions.txt', \
+    deg_corpus_test,sga2deg_test,sga_corpus_test = extract_ground_in(dest+'/pathway_origin/test.solutions.txt', \
         dest+'/pathway_origin/sga2deg_test', dest+'/pathway_ground/test.examples', False)
-    deg_corpus_remain,sga2deg_remain = extract_ground_in(dest+'/pathway_origin/remain.solutions.txt', \
+    deg_corpus_remain,sga2deg_remain,sga_corpus_remain = extract_ground_in(dest+'/pathway_origin/remain.solutions.txt', \
         dest+'/pathway_origin/sga2deg_remain', dest+'/pathway_ground/remain.examples', False)
 
-    print 'result!'
-    print len(deg_corpus_train),len(deg_corpus_test),len(deg_corpus_remain)
-    print len(deg_corpus_train.intersection(deg_corpus_test)),len(deg_corpus_train.intersection(deg_corpus_remain))
+    print '_'*30
+    print 'size of deg corpus:'
+    print 'train\ttest\tremain'
+    print '{}\t{}\t{}'.format(len(deg_corpus_train),len(deg_corpus_test),len(deg_corpus_remain))
+    print 'train&test\ttrain&remain'
+    print '{}\t{}'.format(len(deg_corpus_train.intersection(deg_corpus_test)), \
+        len(deg_corpus_train.intersection(deg_corpus_remain)))
 
-    print len(sga2deg_train),len(sga2deg_test),len(sga2deg_remain)
-    print len(sga2deg_train.intersection(sga2deg_test)),len(sga2deg_train.intersection(sga2deg_remain))
+    print '_'*30
+    print 'size of sga2deg:'
+    print 'train\ttest\tremain'
+    print '{}\t{}\t{}'.format(len(sga2deg_train),len(sga2deg_test),len(sga2deg_remain))
+    print 'train&test\ttrain&remain'
+    print '{}\t{}'.format(len(sga2deg_train.intersection(sga2deg_test)), \
+        len(sga2deg_train.intersection(sga2deg_remain)))
+
+    print '_'*30
+    print 'size of sga corpus:'
+    print 'train\ttest\tremain'
+    print '{}\t{}\t{}'.format(len(sga_corpus_train),len(sga_corpus_test),len(sga_corpus_remain))
+    print 'train&test\ttrain&remain'
+    print '{}\t{}'.format(len(sga_corpus_train.intersection(sga_corpus_test)), \
+        len(sga_corpus_train.intersection(sga_corpus_remain)))
+
 
     print 'Done!'
     # Q.E.D.
