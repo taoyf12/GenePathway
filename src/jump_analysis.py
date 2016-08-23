4da7fac0e963984ef2ea640c7c272a6453d95d72
Yifeng Tao <taoyf12@sina.com>
2016-08-23 01:12:48 -0400

    apply bfs

--- src/jump_analysis.py
+++ src/jump_analysis.py
@@ -23,39 +23,57 @@ def readtruth(path):
         values = line.strip().split('\t')
         src = values[0]
         dst = values[1]
-        truth[(values[0],values[1])] = 100
+        truth[(values[0],values[1])] = 1000
         #print truth.keys()
     return truth
 
-def myiter(truth, lut, src, src_origin, maxcount, count):
-    count += 1
-    tmp = {}
-    for i in truth.keys():
-        tmp[i] = truth[i]
-    truth = {}
-    for i in tmp.keys():
-        truth[i] = tmp[i]
 
-    if count == 5: return truth
-    for dst in lut[src]:
-        # if (src_origin, dst) not in truth: continue
-        if (src_origin, dst) in truth.keys():
-            # print (src_origin,dst)
-            tmp = truth[(src_origin,dst)]
-            if count < tmp:
-                truth[(src_origin,dst)] = count
-        src = dst
-        #truth = dd(int)
-        truth = myiter(truth, lut, src, src_origin, maxcount, count)
+def bfs(truth, lut):
+    dist = {}
+    # distance from root to each node.
+    #i = 0
+    for t in truth.keys():
+        #i += 1
+        #if i == 2: break
+        #print t
+        dist = {}
+        src = t[0]
+        dst = t[1]
+        queue = list()
+        queue.append(src)
+        dist[src] = 0
+        #print src, dst
+        flag = 0
+        while len(queue) != 0:
+            if flag == 1:
+                flag = 0
+                break
+            current = queue[0]
+            #print queue
+            queue = queue[1:]
+            for n in lut[current]:
+                dist[n] = dist[current] + 1
+                queue.append(n)
+                #print n
+                if n == dst:
+                    truth[(src,dst)] = dist[n]
+                    flag = 1
+                    break
+    return truth
 
-def count_jump(truth, lut, maxcount):
-    src_set = set()
-    for t in truth:
-        src_set.add(t[0])
-    for src_origin in src_set:
-        src = src_origin
-        count = 0
-        truth = myiter(truth, lut, src, src_origin, maxcount, count)
+
+
+
+
+
+
+    # src_set = set()
+    # for t in truth:
+    #     src_set.add(t[0])
+    # for src_origin in src_set:
+    #     src = src_origin
+    #     count = 0
+    #     truth = myiter(truth, lut, src, src_origin, maxcount, count)
     return truth
 
 
@@ -64,14 +82,12 @@ if __name__ == '__main__':
     dest = '../pathway_forw_patient'
 
     lut = readpathway(dest+'/pathway_ground/pathway.graph', 1, 2)
-
-
     truth = readtruth(dest+'/pathway_ground/sga2deg_remain')
 
-    jump = count_jump(truth, lut, 5)
+    truth = bfs(truth, lut)
 
-    for t in jump.keys():
-        print t, jump[t]
+    for t in truth.keys():
+        print '{}\t{}'.format(t, truth[t])
 
 
     print 'Done!'
