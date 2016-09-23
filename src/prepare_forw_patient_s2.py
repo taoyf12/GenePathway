1dc2f9171eb3bf33f9e5f2f99899108490be80c0
Yifeng Tao <taoyf12@sina.com>
2016-09-22 21:37:41 -0400

    generate examples

--- src/prepare_forw_patient_s2.py
+++ src/prepare_forw_patient_s2.py
@@ -1,35 +1,38 @@
-# Prepare date from raw data.
-# prepare_forw_patient.py: separate the train, test dataset, and produce graph
-# in a patient-wise manner.
+# generate example files.
+# generate in the way of weight(SGA -> DEG) = \sum (posterior prob) / Number_of_tumors_with_SGA
+
 from collections import defaultdict as dd
 import io
 import os
 import random
 
-# prepare the data for our experiments.
-
-def extract_sga2deg(path_sga2deg_all,path_sga2deg_train,thresh):
+def get_sga2deg(path_sga2deg_all, path_sga2deg_train, patid_train):
     print 'reading from: {}...'.format(path_sga2deg_all)
-    sga2deg = []
+    sga2deg = dd(float)
     sga2deg_str = []
-    sga2deg_all_list = []
+    sga2deg_all_list = set()
     sga_corpus = set()
     deg_corpus = set()
     for line in open(path_sga2deg_all, 'r'):
         values = line.strip().split('\t')
-        sga,deg,prob = values[1],values[2],float(values[3])
-        sga2deg_all_list.append((sga,deg))
-        if prob > thresh:
-            sga2deg.append((sga,deg,prob))
-            sga_corpus.add(sga)
-            deg_corpus.add(deg)
-            sga2deg_str.append('\t'.join(values))
+        patid,sga,deg,prob = int(values[0]),values[1],values[2],float(values[3])
+        sga2deg_all_list.add((sga,deg))
+        if patid not in patid_train: continue
+        sga2deg[(sga,deg)]+= prob
+        sga_corpus.add(sga)
+        deg_corpus.add(deg)
+        #sga2deg_str.append('\t'.join(values))
+
+    for row in sga2deg.keys():
+        sga2deg_str.append('leadTo'+'\t'+row[0]+'\t'+row[1]+'\t'+str(sga2deg[row]))
 
     print 'saving to {}...'.format(path_sga2deg_train)
     f = open(path_sga2deg_train,'w')
     for row in sga2deg_str:
         print >> f, row
     f.close
+
+    sga2deg = sga2deg.keys()
     return sga2deg,sga_corpus,deg_corpus,sga2deg_all_list
 
 def save2txt_list(path, table):
@@ -83,45 +86,52 @@ def writeSample(path, filename, sga2deglist, deg_corpus):
 
 if __name__ == '__main__':
 
-    print 'preparing training set'
+    print 'preparing training and test set...'
 
     root = '/usr1/public/yifeng/GenePathway'
-    dest = root+'/pathway_forw_patient'
+    dest = root+'/pathway'
 
 
-    # get the training(?) edges.
+    deg_corpus_train = set()
+    path_deg_corpus_train = dest+'/labels.cfacts'
+    print 'reading from: {}...'.format(path_deg_corpus_train)
+    for line in open(path_deg_corpus_train, 'r'):
+        values = line.strip().split('\t')
+        deg_corpus_train.add(values[1])
 
-    path_sga2deg_all = dest+'/pathway_processed/pathway_prob.graph'
-    path_sga2deg_train = dest+'/pathway_origin/pathway.graph'
-    thresh = 7
-    sga2deg,sga_corpus,deg_corpus,sga2deg_all_list = extract_sga2deg(path_sga2deg_all,path_sga2deg_train,thresh)
-    print 'sga2deg,sga,deg'
-    print len(sga2deg),len(sga_corpus),len(deg_corpus)
-    print 1.0*len(sga2deg)/1496128,1.0*len(sga_corpus)/9829,1.0*len(deg_corpus)/5776
+    # path_train_example = dest+'/pathway_origin/train.examples'
 
 
-    # get the labels.cfacts
-    path_label = dest+'/pathway_origin/labels.cfacts'
-    print 'saving to {}...'.format(path_label)
-    f = open(path_label,'w')
-    for deg in deg_corpus:
-        print >> f, 'isDEG\t'+deg
-    f.close
+    sga2deglist_train = dd(list)
+    path_train = dest+'/train'
+    print 'reading from: {}...'.format(path_train)
+    for line in open(path_train, 'r'):
+        values = line.strip().split('\t')
+        sga,deg,prob = values[0],values[1],float(values[2])
+        sga2deglist_train[sga].append(deg)
+        # sga2deg_all_list.add((sga,deg))
+
+    writeSample(dest, 'train.examples', sga2deglist_train, deg_corpus_train)
 
     # generate the testing set.
 
 
-    sga2deg_all = dd(list)
-    print 'len(sga2deg_all)={}'.format(len(sga2deg_all_list))
+    deg_corpus_test = set()
+    path_deg_corpus_test = dest+'/labels.cfacts'
 
-    for line in sga2deg_all_list:
-        sga = line[0]
-        deg = line[1]
-        sga2deg_all[sga].append(deg)
+    sga2deglist_test = dd(list)
+    path_test= dest+'/test'
+    print 'reading from: {}...'.format(path_test)
+    for line in open(path_test, 'r'):
+        values = line.strip().split('\t')
+        sga,deg,prob = values[0],values[1],float(values[2])
+        sga2deglist_test[sga].append(deg)
+        deg_corpus_test.add(deg)
+        # sga2deg_all_list.add((sga,deg))
 
-    writeSample(dest+'/pathway_origin', 'test.examples', sga2deg_all, deg_corpus)
+    writeSample(dest, 'test.examples', sga2deglist_test, deg_corpus_test)
 
-
+    # 
 
     print 'Done!'
-    # Q.E.D.
+# Q.E.D.
